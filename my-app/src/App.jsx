f2fa7f46648a81cdfe26943110d55c797f96151e
Sehaj Naangal <snaangal@uwo.ca>
2023-01-12 19:56:11 -0500

    get all data for location and display in html page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Location from './components/Location/Location';
+import locationData from './locationData';
 
 export default function App() {
+    const data = locationData.map((location) => {
+        return <Location 
+            key={location.id}
+            title={location.title}
+            location={location.location}
+            mapsUrl={location.googleMapsUrl}
+            startDate={location.startDate}
+            endDate={location.endDate}
+            desc={location.description}
+            imageUrl={location.imageUrl}
+            mapsIcon={location.mapsIcon}
+        />
+    })
     return (
         <div>
             <Navbar />
-            <Location />
+            {data}
         </div>
     )
 }
--- my-app/src/components/Location/Location.jsx
+++ my-app/src/components/Location/Location.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './location.css'
 
-export default function Location() {
+export default function Location(props) {
     return (
         <div className='location'>
-            <img src="" alt="" />
+            <img src={props.imageUrl} alt="" />
+            <img src={props.mapsIcon} alt="" />
+            <h3>{props.location}</h3>
+            <a href={props.mapsUrl}>View on Google Maps</a>
+            <h1>{props.title}</h1>
+            <h2>{`${props.startDate} - ${props.endDate}`}</h2>
+            <p>{props.desc}</p>
         </div>
     )
 }
